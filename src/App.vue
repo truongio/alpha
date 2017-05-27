26be42d8cb6a4b13cf6fc2ccf81bc04397b4aee9
truongio <truonghoang9011@gmail.com>
2017-05-27 10:45:03 +0200

    Fixed side navigation

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,70 @@
 <template>
   <div id="app">
     <md-toolbar>
-      <md-button class="md-icon-button">
+      <md-button class="md-icon-button" @click.native="toggleLeftSidenav">
         <md-icon>menu</md-icon>
       </md-button>
   
       <h2 class="md-title" style="flex: 1">Alpha</h2>
     </md-toolbar>
-
+  
+    <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
+      <md-toolbar class="md-large">
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Sidenav content</h3>
+        </div>
+      </md-toolbar>
+  
+      <md-list>
+        <md-list-item>
+          <md-icon>whatshot</md-icon>
+          <span>News</span>
+  
+          <md-list-expand>
+            <md-list>
+              <md-list-item class="md-inset">World</md-list-item>
+              <md-list-item class="md-inset">Americas</md-list-item>
+              <md-list-item class="md-inset">Europe</md-list-item>
+            </md-list>
+          </md-list-expand>
+        </md-list-item>
+  
+        <md-list-item>
+          <md-icon>videogame_asset</md-icon>
+          <span>Games</span>
+  
+          <md-list-expand>
+            <md-list>
+              <md-list-item class="md-inset">Console</md-list-item>
+              <md-list-item class="md-inset">PC</md-list-item>
+              <md-list-item class="md-inset">Phone</md-list-item>
+            </md-list>
+          </md-list-expand>
+        </md-list-item>
+  
+        <md-list-item>
+          <md-icon>video_library</md-icon>
+          <span>Video</span>
+  
+          <md-list-expand>
+            <md-list>
+              <md-list-item class="md-inset">Humor</md-list-item>
+              <md-list-item class="md-inset">Music</md-list-item>
+              <md-list-item class="md-inset">Movies</md-list-item>
+              <md-list-item class="md-inset">TV Shows</md-list-item>
+            </md-list>
+          </md-list-expand>
+        </md-list-item>
+  
+        <md-list-item>
+          <md-icon>shopping_basket</md-icon>
+          <span>Shop</span>
+        </md-list-item>
+      </md-list>
+    </md-sidenav>
     <main>
       <div class="md-display-3">Alpha</div>
-      <img src="./assets/logo.png" width="128px"/>
+      <img src="./assets/logo.png" width="128px" />
       <router-view></router-view>
     </main>
   </div>
@@ -18,47 +72,24 @@
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  methods: {
+    toggleLeftSidenav() {
+      this.$refs.leftSidenav.toggle()
+    },
+    toggleRightSidenav() {
+      this.$refs.rightSidenav.toggle()
+    },
+    closeRightSidenav() {
+      this.$refs.rightSidenav.close()
+    }
+  }
 }
 </script>
 
 <style>
-body {
-  margin: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
 main {
   text-align: center;
   margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #4fc08d;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
-
-.md-display-3 {
-  font-weight: 600;
 }
 </style>
