9cb21a46c89e27eb4365556c32aef7d53e53429e
truongio <truonghoang9011@gmail.com>
2017-06-02 23:25:33 +0200

    Fixed sidenav links and added user icon to toolbar

--- src/App.vue
+++ src/App.vue
@@ -7,60 +7,47 @@
       <h2 class="md-title" style="flex: 1">
         <a style="color:white" href="#/">Alpha</a>
       </h2>
-      <md-button class="md-raised md-accent" @click.native="toCreate">New Account</md-button>
+      <md-button @click.native="toCreate">New Account</md-button>
+  
+      <md-menu md-size="4" md-direction="bottom left" :md-offset-x="-10" md-offset-y="50" ref="menu">
+        <md-button class="md-icon-button" md-menu-trigger>
+          <md-icon>person</md-icon>
+        </md-button>
+  
+        <md-menu-content>
+          <div class="author-card">
+            <md-avatar class="md-large">
+              <img src="http://i.imgur.com/hdp9Eil.jpg" alt="Marcos Moura">
+            </md-avatar>
+  
+            <div class="author-card-info">
+              <span>Marcos Moura</span>
+              <div class="author-card-links">
+                <a href="https://linkedin.com/in/marcosvmmoura" target="_blank" rel="noopener">Linkedin</a>
+                <a href="https://github.com/marcosmoura" target="_blank" rel="noopener">GitHub</a>
+              </div>
+            </div>
+          </div>
+        </md-menu-content>
+      </md-menu>
+      </md-button>
     </md-toolbar>
   
-    <md-sidenav :md-swipeable="true" class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
+    <md-sidenav :md-swipeable="true" class="md-left" ref="leftSidenav">
       <md-toolbar class="md-large">
         <md-layout md-align="center" class="md-toolbar-container">
-          <img src="http://materialdesignblog.com/wp-content/uploads/2015/10/2-ionic-material.png" width="80px" />
+          <img src="https://vuejs.org/images/logo.png" width="80px" />
         </md-layout>
       </md-toolbar>
   
       <md-list>
-        <md-list-item>
-          <md-icon>whatshot</md-icon>
-          <span>News</span>
-  
-          <md-list-expand>
-            <md-list>
-              <md-list-item class="md-inset">World</md-list-item>
-              <md-list-item class="md-inset">Americas</md-list-item>
-              <md-list-item class="md-inset">Europe</md-list-item>
-            </md-list>
-          </md-list-expand>
+        <md-list-item @click.native="toHome">
+          <md-icon>home</md-icon>
+          <span>Home</span>
         </md-list-item>
-  
-        <md-list-item>
-          <md-icon>videogame_asset</md-icon>
-          <span>Games</span>
-  
-          <md-list-expand>
-            <md-list>
-              <md-list-item class="md-inset">Console</md-list-item>
-              <md-list-item class="md-inset">PC</md-list-item>
-              <md-list-item class="md-inset">Phone</md-list-item>
-            </md-list>
-          </md-list-expand>
-        </md-list-item>
-  
-        <md-list-item>
-          <md-icon>video_library</md-icon>
-          <span>Video</span>
-  
-          <md-list-expand>
-            <md-list>
-              <md-list-item class="md-inset">Humor</md-list-item>
-              <md-list-item class="md-inset">Music</md-list-item>
-              <md-list-item class="md-inset">Movies</md-list-item>
-              <md-list-item class="md-inset">TV Shows</md-list-item>
-            </md-list>
-          </md-list-expand>
-        </md-list-item>
-  
-        <md-list-item>
-          <md-icon>shopping_basket</md-icon>
-          <span>Shop</span>
+        <md-list-item @click.native="toCreate">
+          <md-icon>create</md-icon>
+          <span>New Account</span>
         </md-list-item>
       </md-list>
     </md-sidenav>
@@ -78,14 +65,13 @@ export default {
     toggleLeftSidenav() {
       this.$refs.leftSidenav.toggle()
     },
-    toggleRightSidenav() {
-      this.$refs.rightSidenav.toggle()
-    },
-    closeRightSidenav() {
-      this.$refs.rightSidenav.close()
-    },
     toCreate() {
       this.$router.push('/create-person-form')
+      this.$refs.leftSidenav.close()
+    },
+    toHome() {
+      this.$router.push('/')
+      this.$refs.leftSidenav.close()
     }
   }
 }
@@ -103,5 +89,33 @@ main {
   display: flex;
   align-items: center;
   align-self: flex-start;
+}
+
+.author-card {
+  padding: 8px 16px;
+  display: flex;
+  align-items: center;
+
+  .md-avatar {
+    margin-right: 16px;
+  }
+
+  .author-card-info {
+    display: flex;
+    flex-flow: column;
+    flex: 1;
+  }
+
+  span {
+    font-size: 16px;
+  }
+
+  .author-card-links {
+    display: flex;
+
+    a+a {
+      margin-left: 8px;
+    }
+  }
 }
 </style>
