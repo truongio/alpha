28cfe337107147c1e66055be70c8de167aa05e1e
truongio <truonghoang9011@gmail.com>
2017-06-03 15:58:55 +0200

    I'm bad at Flexbox LOL. Fixed Safari not wrapping cards

--- src/components/SearchResultGrid.vue
+++ src/components/SearchResultGrid.vue
@@ -1,32 +1,28 @@
 <template>
-  <md-layout class="search-result-grid">
-    <md-layout md-align="center">
-      <md-layout md-flex-small="90" md-flex-large="75" md-flex-medium="100" md-flex="50">
-        <md-layout md-align="center" md-flex="100" v-if="searchResult.length == 0">
-          <md-spinner md-indeterminate></md-spinner>
-        </md-layout>
-        <transition name="fade">
-          <md-layout md-align="center" v-if="searchResult.length > 0">
-            <md-layout class="no-flex" v-for="person in searchResult" :key="person['.key']" v-if="equalsTo(person)">
-              <md-card md-with-hover @click.native="toInfo(person['.key'])">
-                <md-layout>
-                  <md-layout md-align="center">
-                    <md-avatar class="avatar">
-                      <img src="http://i.imgur.com/1rTODyG.jpg" alt="Avatar">
-                    </md-avatar>
-                  </md-layout>
-                  <md-layout md-flex="100" md-align="center">
-                    <div class="md-title">{{person.name}}</div>
-                  </md-layout>
-                  <md-layout md-flex="100" md-align="center">
-                    <div class="md-subhead">{{person.city}}, {{person.country}}</div>
-                  </md-layout>
-                </md-layout>
-              </md-card>
-            </md-layout>
-          </md-layout>
-        </transition>
+  <md-layout class="search-result-grid" md-align="center">
+    <md-layout md-flex-small="90" md-flex-large="75" md-flex-medium="100" md-flex="50">
+      <md-layout md-align="center" md-flex="100" v-if="searchResult.length == 0">
+        <md-spinner md-indeterminate></md-spinner>
       </md-layout>
+      <transition name="fade">
+        <md-layout md-align="center" v-if="searchResult.length > 0">
+          <md-card md-with-hover @click.native="toInfo(person['.key'])" v-for="person in searchResult" :key="person['.key']" v-if="equalsTo(person)">
+            <md-layout>
+              <md-layout md-flex="100" md-align="center">
+                <md-avatar class="avatar">
+                  <img src="http://i.imgur.com/1rTODyG.jpg" alt="Avatar">
+                </md-avatar>
+              </md-layout>
+              <md-layout md-flex="100" md-align="center">
+                <div class="md-title">{{person.name}}</div>
+              </md-layout>
+              <md-layout md-flex="100" md-align="center">
+                <div class="md-subhead">{{person.city}}, {{person.country}}</div>
+              </md-layout>
+            </md-layout>
+          </md-card>
+        </md-layout>
+      </transition>
     </md-layout>
   </md-layout>
 </template>
@@ -80,10 +76,6 @@ a {
 
 .avatar {
   margin: 8px;
-}
-
-.no-flex {
-  flex: 0;
 }
 
 .fade-enter-active,
